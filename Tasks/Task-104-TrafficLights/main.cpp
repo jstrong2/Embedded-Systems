32fabef43b8ddf14a4df52669072203d06c6a466
jstrong2 <joshua.strong@students.plymouth.ac.uk>
2023-10-19 16:27:48 +0100

    traffic lights

--- Tasks/Task-104-TrafficLights/main.cpp
+++ Tasks/Task-104-TrafficLights/main.cpp
@@ -2,6 +2,8 @@
 using namespace uop_msb;
 
 #define WAIT_TIME_MS 500 
+#define SECONDS 1000000
+
 
 DigitalOut red(TRAF_RED1_PIN,1);         //Note the initial state
 DigitalOut amber(TRAF_YEL1_PIN,0);
@@ -11,27 +13,28 @@ LCD_16X2_DISPLAY lcd;
 
 int main()
 {
-    lcd.puts("RED");
-    wait_us(10000000);
-
-    amber = 1;
+  
+  while(true)
+  {
+    red = 1; amber = 0; green = 0;
     lcd.cls();
-    lcd.puts("Amber");
-    wait_us(12000000);
+    lcd.puts("Red");
+    wait_us(10*SECONDS);
 
-    green = 1;
+    red = 1; amber = 1; green = 0;
+    lcd.cls();
+    lcd.puts("Red + Amber");
+    wait_us(2*SECONDS);
+
+    red = 0; amber = 0; green = 1;
     lcd.cls();
     lcd.puts("Green");    
-    wait_us(20000000);
+    wait_us(10*SECONDS);
 
+    red = 0; amber = 1; green = 0;
     lcd.cls();
-    lcd.puts("TASK-104");
+    lcd.puts("Amber");
+    wait_us(2*SECONDS);
 
-    while (true)
-    {
-        red = !red;
-        amber = !amber;
-        green = !green;
-        wait_us(WAIT_TIME_MS * 1000);
-    }
+  }
 }
